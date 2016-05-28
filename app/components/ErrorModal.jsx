e7d8fcf7f63e4576968ab2095c4b10eeb9df6a9a
Renan <renandf@gmail.com>
2016-05-28 19:58:06 +1000

    remove debugger

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -25,7 +25,7 @@ var ErrorModal = React.createClass({
 				</button>
 			</div>
 		);
-		debugger;
+		
 		var $modal = $(ReactDOMServer.renderToString(modalMarkup));
 		$(ReactDOM.findDOMNode(this)).html($modal);
 

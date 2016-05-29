9b892d14bb51ff5c9dc26934b81534586c8cafcd
Renan <renandf@gmail.com>
2016-05-29 11:16:13 +1000

    fixed typo on webpack.config

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,7 @@ var Examples = require('Examples');
 
 // Load Foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
 
 // App css
 require('style!css!sass!applicationStyles');
